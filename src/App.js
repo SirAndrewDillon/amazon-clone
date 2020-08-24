63437d62cc979147990b256989bdc173921b6cae
andrew dillon <majik895@gmail.com>
2020-08-24 17:05:05 -0400

    navbar almost donr

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import './Header.css'
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import './App.css';
+import Header from './Header'
+
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>APP</h1>
+      <Switch>
+        <Route path='/checkout'> 
+        <h1>Checkout</h1>
+      </Route>
+      <Route path='/login'> 
+        <h1>Login Page</h1>
+      </Route>
+      {/* This is the default route */}
+      <Route path='/'> 
+      <Header />
+        <h1>Home Page</h1>
+      </Route>
+        
+      </Switch>
     </div>
+    </Router>
   );
 }
 
