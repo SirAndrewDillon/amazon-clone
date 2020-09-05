14bdb2caa9b30ede43e4f998525df5cb134e0b24
andrew dillon <majik895@gmail.com>
2020-09-05 11:17:50 -0400

    getting sign in working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
+import React, { useEffect } from 'react';
 import "./Header.css";
-import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./Header";
 import Home from './Home'
 import Checkout from './Checkout'
 import Login from './Login'
+import { useStateValue } from './StateProvider';
+import {auth} from './firebase'
 
 function App() {
+  const [{ basket }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if(authUser){
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }else{
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+
+    return () => {
+      unsubscribe();
+    }
+
+  }, [])
   return (
     <Router>
       <div className="App">
--- src/Login.js
+++ src/Login.js
@@ -1,24 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
-import {Link, useHistory} from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
+import { auth } from './firebase'
 
-function Login() {
+const Login = () => {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = (e) => {
+        e.preventDefault();
+        auth.signInWithEmailAndPassword(email, password)
+            .then((auth) => {
+                history.push("/");
+            })
+            .catch(err => alert(err.message));
+    }
+
+    const register = (e) => {
+        e.preventDefault();
+        auth.createUserWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push("/");
+            })
+            .catch(err => alert(err.message));
+    }
+
     return (
         <div className='login'>
            <Link to='/'>
-               <img className='login__logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt="signin pic"/>
-           </Link> 
-           <div className="login__container">
+               <img 
+               className='login__logo'
+                src='https://upload.wikimedia.org/wikipedia/commons/a/a9/Amazon_logo.svg'
+                alt='logo'
+               />
+           </Link>
+           <div className='login__container'>
                <h1>Sign In</h1>
                <form>
-                <h5>E-Mail</h5>
-                <input type="text"/>
-                <h5>Password</h5>
-                <input type="text"/>
-                <button className='login__signInButton'>Sign In</button>
+                   <h5>E-mail</h5>
+                   <input 
+                        value={email}
+                        type='email' 
+                        placeholder='Enter your email' 
+                        required
+                        onChange={e => setEmail(e.target.value)}
+                    />
+
+                   <h5>Password</h5>
+                   <input 
+                        value={password} 
+                        onChange={e => setPassword(e.target.value)}
+                        type='password' 
+                        placeholder='Enter your email' 
+                        required 
+                    />
+
+                   <button onClick={login} type='submit' className='login__signInButton'>Sign In</button>
                </form>
-               <p>By signing-in you agree to Amazon's Conditions of Use and Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.</p>
-               <button className='login__registerButton'>Create your Amazon Account</button>
+               <p>
+                   By signing-in you agree to Amazon's Conditions of Use & Sale. 
+                   Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+               </p>
+               <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
            </div>
         </div>
     )
