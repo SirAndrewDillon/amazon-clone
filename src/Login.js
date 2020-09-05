b937c8331ae437be2cbd4cda19ba12f6af84d731
andrew dillon <majik895@gmail.com>
2020-09-05 10:03:11 -0400

    styling complete

--- src/Login.js
+++ src/Login.js
@@ -15,10 +15,10 @@ function Login() {
                 <input type="text"/>
                 <h5>Password</h5>
                 <input type="text"/>
-                <button>Sign In</button>
+                <button className='login__signInButton'>Sign In</button>
                </form>
                <p>By signing-in you agree to Amazon's Conditions of Use and Sale. Please see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.</p>
-               <button>Create your Amazon Account</button>
+               <button className='login__registerButton'>Create your Amazon Account</button>
            </div>
         </div>
     )
